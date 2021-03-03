2dc0bd8bd221e1773298122e8f8e378fc531288d
purbayankar <53092711+purbayankar@users.noreply.github.com>
2021-03-04 01:05:26 +0530

    Update SSRN_IN.py

--- SSRN_IN.py
+++ SSRN_IN.py
@@ -5,6 +5,7 @@ import scipy.io as sio
 from keras.models import Sequential, Model
 from keras.layers import Convolution2D, MaxPooling2D, Conv3D, MaxPooling3D, ZeroPadding3D, Conv2D, Flatten, Dense, Reshape, BatchNormalization
 from keras.layers import Activation, Dropout, Flatten, Dense, BatchNormalization, Input, AveragePooling2D
+from keras.layers import *
 from keras.utils.np_utils import to_categorical
 from sklearn.decomposition import PCA
 from keras.optimizers import Adam, SGD, Adadelta, RMSprop, Nadam
